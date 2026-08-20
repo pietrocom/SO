a824d425d1178dfe364b9a68aadbeee106208745
pietrocom <pietro.comin@ufpr.br>
2026-08-20 14:56:50 -0300

    [add] Progresso na implementação das tasks

--- kernel/task.c
+++ kernel/task.c
@@ -4,11 +4,63 @@
 
 // Gerência básica de tarefas.
 
-void task_init()
-{
+
+// --- Includes ---
+
+#include "tcb.h"
+#include "task.h"
+#include "ctx.h"
+#include "memory.h"
+#include "queue.h"
+#include "macros.h"
+
+
+// --- Variaveis Globais ---
+
+static int ids;
+struct task_t * current_task;  // Task que esta sendo executada
+struct queue_t * task_queue;
+
+
+// --- Funcoes da API ---
+
+void task_init () {
+    // Inicializacoes
+    ids = 0;
+    current_task = NULL;
+    task_queue = queue_create();
+    if (!task_queue) {
+        ppos_panic();
+        return;
+    }
 }
 
-void task_term()
-{
+void task_term () {
+
 }
 
+struct task_t * task_create (char * name, void (* entry)(void *), void * arg) {
+    if (!entry) return NULL;
+
+    struct task_t * task = mem_alloc(sizeof(struct task_t));
+    if (!task) return NULL;
+
+    void * stack_pointer = mem_alloc(STACK_SIZE);
+    if (!stack_pointer) {
+        mem_free(task);
+
+        return NULL;
+    }
+
+    task->name   = name;
+    task->status = NEW;
+    task->id     = ids;
+    if (ctx_create(&task->context, entry, arg, stack_pointer, STACK_SIZE) == -1) {
+        mem_free(task);
+        mem_free(stack_pointer);
+        
+        return NULL;
+    }
+
+    return task;
+}
--- kernel/tcb.h
+++ kernel/tcb.h
@@ -9,14 +9,25 @@
 #ifndef __PPOS_TCB__
 #define __PPOS_TCB__
 
+#define STACK_SIZE 32 * 1024
+
+#include "ctx.h"
+
+typedef enum {
+    NEW =        0,
+    READY =      1,
+    RUNNING =    2,
+    SUSPENDED =  3,
+    TERMINATED = 4
+} Status;
+
 // Task Control Block (TCB), infos sobre uma tarefa
 struct task_t
 {
-    int id;         // identificador da tarefa
-    char *name;     // nome da tarefa
-    ctx_t context;  // contexto da tarefa
-    int status;     // pronta, executando, ...
-    ...             // demais informações, a completar
+    int id;                // identificador da tarefa
+    char * name;           // nome da tarefa
+    struct ctx_t context;  // contexto da tarefa
+    Status status;         // pronta, executando, ...
 };
 
 #endif
